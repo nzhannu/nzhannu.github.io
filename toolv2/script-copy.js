b6a591485f84f8793fb629e331b617d4a8414e6b
Hannu <117394554+nzhannu@users.noreply.github.com>
2023-09-15 09:28:12 +1200

    All in one tool

---
+++ toolv2/script-copy.js
@@ -0,0 +1,9 @@
+function copyData() {
+            let copyGfGText =
+                document.getElementById("prefix-suffix-lines-text");
+ 
+            copyGfGText.select();
+            document.execCommand("copy");
+             
+         
+        }
